69b5a9177d7b8c3ba04dbee9c6075b522429ed39
EdwardYangZ <604582742@qq.com>
2019-01-03 16:14:31 +0800

    提交遗漏文件

---
+++ kbe_assets/scripts/data/cfg_room.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+''' 房间游戏 className 对应玩家 className '''
+game_player_classname = {
+    "GameDdz": "PlayerDdz"
+}
+
+''' 游戏至少座位数 '''
+game_seat_min = {
+    "GameDdz": 3
+}
+
+''' 等级底分和入场金币限制 '''
+game_level = [
+    {
+        'baseScore': 10,
+        'goldMin': 1000
+    },
+    {
+        'baseScore': 100,
+        'goldMin': 10000
+    },
+    {
+        'baseScore': 1000,
+        'goldMin': 100000
+    },
+]
